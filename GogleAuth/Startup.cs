3709af9c24e37a1d64822021a5f6625eedb7155c
Daniil Neichev <daniil.neichev@outlook.com>
2021-02-02 15:22:32 +0200

    First commit by daddy

---
+++ GogleAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GogleAuth.Startup))]
+namespace GogleAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
